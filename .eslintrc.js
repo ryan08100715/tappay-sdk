a51030d6936a3a71f7c40dcf7ba32a7ad250eca5
ryan <ryan08100715@gmail.com>
2021-11-29 14:05:51 +0800

    update package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,5 @@ module.exports = {
   },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
 };
